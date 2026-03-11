831c29966026768566e7cec60121a87ad19aa6b7
Kunal Solanki <kunalsol2005@gmail.com>
2026-03-11 08:35:10 +0530

    Time: 2 ms (99.15%) | Memory: 44.4 MB (74.51%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,12 +1,24 @@
 class Solution {
+    public char lower(char c){
+        if(c>='a' && c<='z') return c;
+        return (char)((c)-('A'-'a'));
+    }
+    public boolean check(char ch){
+        boolean isLower = (ch >= 'a' && ch <= 'z');
+        boolean isUpper = (ch >= 'A' && ch <= 'Z');
+        boolean isDigit = (ch >= '0' && ch <= '9');
+        return isLower || isUpper || isDigit;
+    }
     public boolean isPalindrome(String s) {
-        int l=0,r=s.length()-1;
-        while(l<r){
-            while(l<r&&!Character.isLetterOrDigit(s.charAt(l))) l++;
-            while(l<r&&!Character.isLetterOrDigit(s.charAt(r))) r--;
-            if(Character.toLowerCase(s.charAt(l))!=Character.toLowerCase(s.charAt(r))) return false;
-            l++;
-            r--;
+        int i=0,j=s.length()-1;
+        while(i<j){
+            while(i<j && !(check(s.charAt(i)))) i++;
+            while(i<j && !(check(s.charAt(j)))) j--;
+            char c = lower(s.charAt(i));
+            char ch = lower(s.charAt(j));
+            if(c!=ch) return false;
+            i++;
+            j--;
         }
         return true;
     }
