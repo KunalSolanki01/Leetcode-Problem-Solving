086970e348dbf425a0595b470b88da198763c074
Kunal Solanki <kunalsol2005@gmail.com>
2026-03-26 11:08:01 +0530

    Time: 5 ms (74.01%) | Memory: 53 MB (24.01%) - LeetSync

--- 1027-sum-of-even-numbers-after-queries/sum-of-even-numbers-after-queries.java
+++ 1027-sum-of-even-numbers-after-queries/sum-of-even-numbers-after-queries.java
@@ -1,18 +1,31 @@
 class Solution {
-    public int even(int[] arr){
-        int sum = 0;
-        for(int i:arr){
-            if(i%2==0) sum+=i;
-        }
-        return sum;
-    }
+    // public int even(int[] arr){
+    //     int sum = 0;
+    //     for(int i:arr){
+    //         if(i%2==0) sum+=i;
+    //     }
+    //     return sum;
+    // }
     public int[] sumEvenAfterQueries(int[] nums, int[][] queries) {
         int[] arr = new int[queries.length];
+        int even = 0;
+        for(int i:nums){
+            if(i%2==0) even+=i;
+        }
         for(int i=0;i<queries.length;i++){
             int idx = queries[i][1];
-            nums[idx]+=queries[i][0];
-            arr[i] = even(nums);
+            int val = queries[i][0];
+            if(nums[idx]%2==0) even-=nums[idx];
+            nums[idx]+=val;
+            if(nums[idx]%2==0) even+=nums[idx];
+            arr[i] = even;
         }
         return arr;
+        // for(int i=0;i<queries.length;i++){
+        //     int idx = queries[i][1];
+        //     nums[idx]+=queries[i][0];
+        //     arr[i] = even(nums);
+        // }
+        // return arr;
     }
 }
