de5ee25c71a2734c1dd2fd1de873761d1620bcf6
Kunal Solanki <kunalsol2005@gmail.com>
2026-04-15 14:09:46 +0530

    Time: 0 ms (100.00%) | Memory: 13.6 MB (92.75%) - LeetSync

--- 825-max-increase-to-keep-city-skyline/max-increase-to-keep-city-skyline.cpp
+++ 825-max-increase-to-keep-city-skyline/max-increase-to-keep-city-skyline.cpp
@@ -7,13 +7,13 @@ public:
     int count = 0;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            row[i] = std::max(row[i], grid[i][j]);
-            col[j] = std::max(col[j], grid[i][j]);
+            row[i] = max(row[i], grid[i][j]);
+            col[j] = max(col[j], grid[i][j]);
         }
     }
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            int height = std::min(row[i], col[j]);
+            int height = min(row[i], col[j]);
             count += height - grid[i][j];
         }
     }
