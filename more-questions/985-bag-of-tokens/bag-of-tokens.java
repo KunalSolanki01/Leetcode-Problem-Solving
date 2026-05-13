847e14c373dd8a13025b34dd6f7a5a7648f38c5c
Kunal Solanki <kunalsol2005@gmail.com>
2026-05-13 08:58:40 +0530

    Time: 40 ms (6.98%) | Memory: 47 MB (9.68%) - LeetSync

---
+++ more-questions/985-bag-of-tokens/bag-of-tokens.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int bagOfTokensScore(int[] tokens, int power) {
+        Arrays.sort(tokens);
+        int ans = 0,max = 0;
+        int n = tokens.length;
+        int j=n-1;
+        int i=0;
+       while(i<=j){
+            if(tokens[i]<=power){
+                ans++;
+                power-=tokens[i++];
+                //max = Math.max(max,ans);
+            }
+            else if(ans>0){
+                ans--;
+                power+=tokens[j--];
+            }
+            else break;
+            System.out.print(power+" ");
+                max = Math.max(max,ans);
+        }
+        return max;
+    }
+}
