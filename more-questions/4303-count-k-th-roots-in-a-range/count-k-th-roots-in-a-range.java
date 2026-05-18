12960920b0b74e8bdfe5eb0f1fa4430940841b12
Kunal Solanki <kunalsol2005@gmail.com>
2026-05-18 11:22:34 +0530

    Time: 3 ms (0.00%) | Memory: 42.7 MB (100.00%) - LeetSync

--- more-questions/4303-count-k-th-roots-in-a-range/count-k-th-roots-in-a-range.java
+++ more-questions/4303-count-k-th-roots-in-a-range/count-k-th-roots-in-a-range.java
@@ -1,11 +1,4 @@
 class Solution {
-    // public boolean check(int x,int k){
-    //     if(x==1) return true;
-    //     while(x%k==0){
-    //         x/=k;
-    //     }
-    //     return x==1;
-    // }
     public int countKthRoots(int l, int r, int k) {
         int count = 0;
         if(k==1) return r-l+1;
