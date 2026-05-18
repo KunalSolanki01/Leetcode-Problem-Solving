68e04b69c08dd1c29dd0c7f9d5701fbd214fb38b
Kunal Solanki <kunalsol2005@gmail.com>
2026-05-18 13:11:11 +0530

    Time: 25 ms (77.72%) | Memory: 55.2 MB (42.61%) - LeetSync

---
+++ more-questions/1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        int n = points.length;
+        int [][]ans = new int[k][2];
+        Arrays.sort(points,(a, b) ->Integer.compare((a[0] * a[0] + a[1] * a[1]), (b[0] * b[0] + b[1] * b[1])));
+        for(int i=0;i<k;i++){
+            ans[i] = points[i];
+        }
+        return ans;
+    }
+}
