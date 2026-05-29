113a27a243a90d9de5e4e4020bffd0b2a83c30dc
Kunal Solanki <kunalsol2005@gmail.com>
2026-05-29 12:02:58 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (72.15%) - LeetSync

---
+++ more-questions/367-valid-perfect-square/valid-perfect-square.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        long l = 0,h = num;
+        while(l<=h){
+            long mid = l+(h-l)/2;
+            long t = mid*mid;
+            if(num==t) return true;
+            else if(num>t) l = mid+1;
+            else h = mid-1;
+        }
+        return false;
+
+
+        // if(num >= 0){
+        //     int a = (int)Math.sqrt(num);
+        //     return (a*a == num);
+        // }
+        // return false;
+
+    }
+}
