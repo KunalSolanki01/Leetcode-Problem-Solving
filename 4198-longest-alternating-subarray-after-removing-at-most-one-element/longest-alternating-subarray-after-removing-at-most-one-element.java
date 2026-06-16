98bfa4b81cd2ef2c0247eb68cbfece0ab7cc5b94
Kunal Solanki <kunalsol2005@gmail.com>
2026-06-16 17:13:03 +0530

    Time: 63 ms (58.14%) | Memory: 169.3 MB (97.67%) - LeetSync

--- 4198-longest-alternating-subarray-after-removing-at-most-one-element/longest-alternating-subarray-after-removing-at-most-one-element.java
+++ 4198-longest-alternating-subarray-after-removing-at-most-one-element/longest-alternating-subarray-after-removing-at-most-one-element.java
@@ -1,17 +1,13 @@
 class Solution {
     public int longestAlternating(int[] nums) {
         int n = nums.length;
-
         if (n == 1) {
             return 1;
         }
-
         int[] L = new int[n];
         int[] R = new int[n];
-
         java.util.Arrays.fill(L, 1);
         java.util.Arrays.fill(R, 1);
-
         for (int i = 1; i < n; i++) {
             if (i == 1) {
                 L[i] = (nums[i] != nums[i - 1]) ? 2 : 1;
@@ -47,37 +43,26 @@ class Solution {
             } else {
                 int lv = L[i - 1];
                 int rv = R[i + 1];
-
                 int d2 = Integer.compare(nums[i + 1], nums[i - 1]);
-
                 if (d2 != 0) {
                     int d1 = (lv > 1)
                             ? Integer.compare(nums[i - 1], nums[i - 2])
                             : 0;
-
                     int d3 = (rv > 1)
                             ? Integer.compare(nums[i + 2], nums[i + 1])
                             : 0;
-
                     if ((d1 == 0 || d1 != d2) &&
                         (d3 == 0 || d2 != d3)) {
-
                         res = Math.max(res, lv + rv);
-
                     } else if (d1 == 0 || d1 != d2) {
-
                         res = Math.max(res, lv + 1);
-
                     } else if (d3 == 0 || d2 != d3) {
-
                         res = Math.max(res, rv + 1);
                     }
                 }
-
                 res = Math.max(res, Math.max(lv, rv));
             }
         }
-
         return res;
     }
 }
