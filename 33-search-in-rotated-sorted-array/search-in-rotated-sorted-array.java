cffd0ce3a83b198c41e7d29d4dae4fafb7f24313
Kunal Solanki <kunalsol2005@gmail.com>
2026-06-18 10:37:59 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (27.05%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,32 +1,46 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int peak = 0;
+        // int peak = 0;
         int n = nums.length;
         int l = 0,h = n-1;
+
         while(l<=h){
             int mid = l+(h-l)/2;
-            if(nums[0]<=nums[mid]){
-                peak = mid;
-                l = mid+1;
+            if(nums[mid]==target) return mid;
+            else if(nums[l]<=nums[mid]) {
+                if(target<nums[mid] && target>=nums[l]) h = mid-1;
+                else l = mid+1;
             }
-            else h = mid-1;
+            else{
+                if(target>nums[mid] && target<=nums[h]) l = mid+1;
+                else h = mid-1;
+            }
         }
-        l = 0;
-        h = peak;
-        while(l<=h){
-            int mid = l+(h-l)/2;
-            if(nums[mid]==target) return mid;
-            else if(nums[mid]>target) h = mid-1;
-            else l = mid+1;
-        }
-        l = peak+1;
-        h = n-1;
-        while(l<=h){
-            int mid = l+(h-l)/2;
-            if(nums[mid]==target) return mid;
-            else if(nums[mid]>target) h = mid-1;
-            else l = mid+1;
-        }
+        
+        // while(l<=h){
+        //     int mid = l+(h-l)/2;
+        //     if(nums[0]<=nums[mid]){
+        //         peak = mid;
+        //         l = mid+1;
+        //     }
+        //     else h = mid-1;
+        // }
+        // l = 0;
+        // h = peak;
+        // while(l<=h){
+        //     int mid = l+(h-l)/2;
+        //     if(nums[mid]==target) return mid;
+        //     else if(nums[mid]>target) h = mid-1;
+        //     else l = mid+1;
+        // }
+        // l = peak+1;
+        // h = n-1;
+        // while(l<=h){
+        //     int mid = l+(h-l)/2;
+        //     if(nums[mid]==target) return mid;
+        //     else if(nums[mid]>target) h = mid-1;
+        //     else l = mid+1;
+        // }
         return -1;
     }
 }
