9cf1f1392475121c6455de276b3f02f74197cad6
Kunal Solanki <kunalsol2005@gmail.com>
2026-06-18 14:56:34 +0530

    Time: 42 ms (15.44%) | Memory: 91.9 MB (67.66%) - LeetSync

--- 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.java
+++ 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.java
@@ -1,22 +1,21 @@
 class Solution {
+    public boolean isP(int []nums,long k,int mid){
+        long cur = 0;
+        for(int i:nums){
+            cur+=(i/mid);
+        }
+        if(cur>=k) return false;
+        return true;
+    }
     public int maximumCandies(int[] candies, long k) {
         int s = 1,e = Integer.MAX_VALUE;
         int n = candies.length;
         int ans = 0;
         while(s<=e){
             int candy = s+(e-s)/2;
-            long cur = 0;
-            for(int i=0;i<n;i++){
-                cur += (candies[i]/candy);
-            }
-            if(k<=cur){
-                s = candy+1;
-                ans = candy;
-            }
-            else{ 
-                e = candy-1;
-            }
+            if(isP(candies,k,candy)) e = candy-1;
+            else s = candy+1;
         }
-        return ans;
+        return e;
     }
 }
