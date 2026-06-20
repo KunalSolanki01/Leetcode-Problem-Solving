1bef0ef91205cc45ea13f1ded3d3d79e2786a47b
Kunal Solanki <kunalsol2005@gmail.com>
2026-06-20 19:36:50 +0530

    Time: 45 ms (58.66%) | Memory: 91.1 MB (17.93%) - LeetSync

---
+++ 164-maximum-gap/maximum-gap.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maximumGap(int[] nums) {
+        int n = nums.length;
+        if(n<2) return 0;
+        Arrays.sort(nums);
+        int gap = 0;
+        for(int i=0;i<n-1;i++){
+            gap = Math.max(gap,(nums[i+1]-nums[i]));
+        }
+        return gap;
+    }
+}
