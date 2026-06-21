6c168c6157078d31aebf52d3644ada7ebafc3aa4
Kunal Solanki <kunalsol2005@gmail.com>
2026-06-21 09:44:00 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (48.08%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.java
+++ 96-unique-binary-search-trees/unique-binary-search-trees.java
@@ -1,13 +1,18 @@
 import java.math.*;
 class Solution {
-    public BigInteger fac(int n){
-        BigInteger ans = BigInteger.ONE;
-        for(int i=2;i<=n;i++){
-            ans=ans.multiply(BigInteger.valueOf(i));
-        }
-        return ans;
-    }
+    // public BigInteger fac(int n){
+    //     BigInteger ans = BigInteger.ONE;
+    //     for(int i=2;i<=n;i++){
+    //         ans=ans.multiply(BigInteger.valueOf(i));
+    //     }
+    //     return ans;
+    // }
     public int numTrees(int n) {
-        return (fac(2*n).divide(fac(n+1).multiply(fac(n)))).intValue();
+        // return (fac(2*n).divide(fac(n+1).multiply(fac(n)))).intValue();
+        long a = 1;
+        for (int i = 0; i < n; i++) {
+            a=a*2*(2*i+1)/(i+2);
+        }
+        return(int)a;
     }
 }
