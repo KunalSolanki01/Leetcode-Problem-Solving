3a7a49295e3da45f34da3e4a72972d773ee79ea0
Kunal Solanki <kunalsol2005@gmail.com>
2026-07-04 09:57:01 +0530

    Time: 759 ms (5.06%) | Memory: 43.1 MB (79.32%) - LeetSync

--- more-question/494-target-sum/target-sum.java
+++ more-question/494-target-sum/target-sum.java
@@ -1,16 +1,23 @@
 class Solution {
     static int count = 0;
-    public void helper(int []nums,int target,int sum,int idx){
-        if(idx>=nums.length){
-            if(sum==target) count++; 
+    public void helper(int []nums,int target,int sum,int idx,int cur){
+        if(idx==nums.length){
+            if((sum+target)%2!=0) return;
+            if(cur==(sum+target)/2) count++;
             return;
         }
-        helper(nums,target,sum+nums[idx],idx+1);
-        helper(nums,target,sum-nums[idx],idx+1);
+        helper(nums,target,sum,idx+1,cur+nums[idx]);
+        helper(nums,target,sum,idx+1,cur);
+        // if(idx>=nums.length){
+        //     if(sum==target) count++; 
+        //     return;
+        // }
+        // helper(nums,target,sum+nums[idx],idx+1);
+        // helper(nums,target,sum-nums[idx],idx+1);
     }
     public int findTargetSumWays(int[] nums, int target) {
         count = 0;
-        helper(nums,target,0,0);
+        helper(nums,target,Arrays.stream(nums).sum(),0,0);
         return count;
     }
 }
