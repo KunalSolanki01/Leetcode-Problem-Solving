c4ff735973538aad6145ba49c81b31c51e6e2fc4
Kunal Solanki <kunalsol2005@gmail.com>
2026-07-06 09:16:45 +0530

    Time: 1 ms (96.32%) | Memory: 42.2 MB (45.06%) - LeetSync

--- more-question/52-n-queens-ii/n-queens-ii.java
+++ more-question/52-n-queens-ii/n-queens-ii.java
@@ -1,12 +1,33 @@
 class Solution {
+    public void helper(List<List<String>> ans,int r,char[][] mat,boolean[] col,boolean[] dig1,boolean[] dig2,int n){
+        if(r==n){
+            List<String> temp = new ArrayList<>();
+            for(char[] arr:mat) temp.add(new String(arr));
+            ans.add(temp);
+            return;
+        }
+        for(int c=0;c<n;c++){
+            int d1 = n+r-c;
+            int d2 = r+c;
+            if(!col[c] && !dig1[d1] && !dig2[d2]){
+                col[c] = dig1[d1] = dig2[d2] = true;
+                mat[r][c] = 'Q';
+                helper(ans,r+1,mat,col,dig1,dig2,n);
+                col[c] = dig1[d1] = dig2[d2] = false;
+                mat[r][c] = '.';
+            }
+        }
+    }
     public int totalNQueens(int n) {
-        if(n==1) return 1;
-        if(n==2 || n==3) return 0;
-        if(n==4) return 2;
-        if(n==5) return 10;
-        if(n==6) return 4;
-        if(n==7) return 40;
-        if(n==8) return 92;
-        return 352;
+        List<List<String>> ans = new ArrayList<>();
+        char [][]mat = new char[n][n];
+        for(int i=0;i<n;i++){
+            Arrays.fill(mat[0],'.');
+        }
+        boolean []col = new boolean[n];
+        boolean []dig1 = new boolean[2*n];
+        boolean []dig2 = new boolean[2*n];
+        helper(ans,0,mat,col,dig1,dig2,n);
+        return ans.size();
     }
 }
