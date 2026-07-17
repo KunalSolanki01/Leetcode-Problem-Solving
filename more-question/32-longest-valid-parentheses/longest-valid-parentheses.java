1894c7543c42c650844ff26f752ff3732e1efd25
Kunal Solanki <kunalsol2005@gmail.com>
2026-07-17 13:31:22 +0530

    Time: 5 ms (82.28%) | Memory: 46.6 MB (42.40%) - LeetSync

--- more-question/32-longest-valid-parentheses/longest-valid-parentheses.java
+++ more-question/32-longest-valid-parentheses/longest-valid-parentheses.java
@@ -4,26 +4,14 @@ class Solution {
         int ans = 0;
         int n = s.length();
         st.push(-1);
-        // int []arr = new int[n];
         for(int i=0;i<n;i++){
             if(s.charAt(i)=='(') st.push(i);
             else{
                 st.pop();
                 if(st.isEmpty()) st.push(i);
-                else{
-                    ans = Math.max(ans,i-st.peek());
-                }
-                // if(st.isEmpty()) continue;
-                // arr[i] = 1;
-                // arr[st.pop()] = 1;
+                else ans = Math.max(ans,i-st.peek());
             }
         }
-        // int sum = 0;
-        // for(int i=0;i<n;i++){
-        //     if(arr[i]==1) sum++;
-        //     else sum = 0;
-        //     ans = Math.max(ans,sum);
-        // }
         return ans;
     }
 }
