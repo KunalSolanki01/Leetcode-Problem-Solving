332b30a1826b0e90b2de1a8f27b0de358a114199
Kunal Solanki <kunalsol2005@gmail.com>
2026-07-19 09:45:54 +0530

    Time: 5 ms (99.90%) | Memory: 71.7 MB (70.98%) - LeetSync

--- more-question/878-shifting-letters/shifting-letters.java
+++ more-question/878-shifting-letters/shifting-letters.java
@@ -1,18 +1,26 @@
 class Solution {
     public String shiftingLetters(String s, int[] shifts) {
-        int n = shifts.length;
-        int[] arr = new int[n];
-        long sum = 0;
-        for(int i=n-1;i>=0;i--){
-            sum+=shifts[i];
-            arr[i] = (int)(sum%26);
+
+        char[] ch = s.toCharArray();
+        long shiftSum =0;
+        for(int i = shifts.length-1; i >=0 ; i--){
+            shiftSum  += shifts[i];
+            ch[i] = (char)('a' + (ch[i] -'a' + shiftSum) % 26);
         }
-        StringBuilder sb = new StringBuilder();
-        for(int i=0;i<n;i++){
-            int val = s.charAt(i)-'a';
-            int newVal = (val+arr[i])%26;
-            sb.append((char)('a'+newVal));
-        }
-        return sb.toString();
+        return new String(ch);
+        // int n = shifts.length;
+        // int[] arr = new int[n];
+        // long sum = 0;
+        // for(int i=n-1;i>=0;i--){
+        //     sum+=shifts[i];
+        //     arr[i] = (int)(sum%26);
+        // }
+        // StringBuilder sb = new StringBuilder();
+        // for(int i=0;i<n;i++){
+        //     int val = s.charAt(i)-'a';
+        //     int newVal = (val+arr[i])%26;
+        //     sb.append((char)('a'+newVal));
+        // }
+        // return sb.toString();
     }
 }
