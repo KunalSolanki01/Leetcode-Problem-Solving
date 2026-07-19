b0523799bc3b8f31f4ad4520986a89b580e0786b
Kunal Solanki <kunalsol2005@gmail.com>
2026-07-19 09:44:44 +0530

    Time: 9 ms (49.26%) | Memory: 72.6 MB (42.65%) - LeetSync

---
+++ more-question/878-shifting-letters/shifting-letters.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String shiftingLetters(String s, int[] shifts) {
+        int n = shifts.length;
+        int[] arr = new int[n];
+        long sum = 0;
+        for(int i=n-1;i>=0;i--){
+            sum+=shifts[i];
+            arr[i] = (int)(sum%26);
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<n;i++){
+            int val = s.charAt(i)-'a';
+            int newVal = (val+arr[i])%26;
+            sb.append((char)('a'+newVal));
+        }
+        return sb.toString();
+    }
+}
