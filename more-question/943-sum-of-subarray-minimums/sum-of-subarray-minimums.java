7813f1ee4e8a2c88c404ddba0445333dfa40f84a
Kunal Solanki <kunalsol2005@gmail.com>
2026-07-21 14:01:47 +0530

    Time: 9 ms (99.73%) | Memory: 72.8 MB (58.04%) - LeetSync

--- more-question/943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
+++ more-question/943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
@@ -1,4 +1,35 @@
 class Solution {
+    public int sumSubarrayMins(int[] arr) {
+        int n = arr.length;
+        int mod = (int)1e9+7;
+        int[] right = new int[n + 1];
+        int[] left = new int[n + 1];
+        int[] sum = new int[n + 1];
+        for (int i = 0; i < n; ++i) {
+            right[i + 1] = arr[i];
+        }
+        int res = 0;
+        for (int i = 1; i <= n; i++) {
+            int curr = right[i];
+            int j = i - 1;
+            while (right[j] >= curr) {
+                j = left[j];
+            } 
+            left[i] = j;
+            sum[i] = sum[j] + curr * (i - j);
+            res = (res + sum[i]) % mod;
+        }
+        return res;
+    }
+        // long mod = 1000000007;
+        // int n = arr.length;
+        // long ans = 0;
+        // int []nse = nse(arr);
+        // int []pse = pse(arr);
+        // for(int i=0;i<n;i++){
+        //     ans = (ans+((((i-pse[i])*(nse[i]-i)%mod)*arr[i])%mod)%mod);
+        // }
+        // return (int)(ans%mod);
     public int[] pse(int []arr){
         Stack<Integer> pse = new Stack<>();
         int n = arr.length;
@@ -22,38 +53,5 @@ class Solution {
             nse.push(i);
         }
         return ans;
-    }
-    public int sumSubarrayMins(int[] arr) {
-        int n = arr.length;
-        int mod = 1000000007;
-        int[] right = new int[n + 1];
-        int[] left = new int[n + 1];
-        int[] sum = new int[n + 1];
-        for (int i = 0; i < n; ++i) {
-            right[i + 1] = arr[i];
-        }
-        int res = 0;
-        for (int i = 1; i <= n; i++) {
-            int curr = right[i];
-            int j = i - 1;
-
-            while (right[j] >= curr) {
-                j = left[j];
-            }
-            
-            left[i] = j;
-            sum[i] = sum[j] + curr * (i - j);
-            res = (res + sum[i]) % mod;
-        }
-        return res;
-        // long mod = 1000000007;
-        // int n = arr.length;
-        // long ans = 0;
-        // int []nse = nse(arr);
-        // int []pse = pse(arr);
-        // for(int i=0;i<n;i++){
-        //     ans = (ans+((((i-pse[i])*(nse[i]-i)%mod)*arr[i])%mod)%mod);
-        // }
-        // return (int)(ans%mod);
     }
 }
