bbd553314d0f17cac8cd3f27372bdd8325b48872
Kunal Solanki <kunalsol2005@gmail.com>
2026-07-23 11:18:36 +0530

    Time: 3 ms (81.38%) | Memory: 43.5 MB (66.22%) - LeetSync

---
+++ more-question/682-baseball-game/baseball-game.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int calPoints(String[] operations) {
+        Stack<Integer> st = new Stack<>();
+        for(String s:operations){
+            if(s.equals("D")){
+                if(!st.isEmpty()) st.push(st.peek()*2);
+            }
+            else if(s.equals("C")){
+                if(!st.isEmpty()) st.pop();
+            }
+            else if(s.equals("+")){
+                if(st.size()>1){
+                    int val = st.get(st.size()-2)+st.peek();
+                    st.push(val);
+                }
+            }
+            else st.push(Integer.parseInt(s));
+        }
+        int sum = 0;
+        for(int i:st) sum+=i;
+        return sum;
+    }
+}
