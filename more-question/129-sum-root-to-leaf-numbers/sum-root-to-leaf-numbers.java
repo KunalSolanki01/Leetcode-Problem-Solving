eb21644a654a67eee1601367f27af1dbed831578
Kunal Solanki <kunalsol2005@gmail.com>
2026-08-14 10:54:08 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (68.12%) - LeetSync

--- more-question/129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.java
+++ more-question/129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.java
@@ -14,22 +14,29 @@
  * }
  */
 class Solution {
-    public void helper(TreeNode root, List<String> ans, String sb){
-        if(root==null) return;
-        if(root.left==null && root.right==null){
-            sb = sb+Integer.toString(root.val);
-            ans.add(sb);
-            return;
-        }
-        helper(root.left,ans,sb+Integer.toString(root.val));
-        helper(root.right,ans,sb+Integer.toString(root.val));
+    // public void helper(TreeNode root, List<String> ans, String sb){
+    //     if(root==null) return;
+    //     if(root.left==null && root.right==null){
+    //         sb = sb+Integer.toString(root.val);
+    //         ans.add(sb);
+    //         return;
+    //     }
+    //     helper(root.left,ans,sb+Integer.toString(root.val));
+    //     helper(root.right,ans,sb+Integer.toString(root.val));
+    // }
+    public int helper(TreeNode root,int sum){
+        if(root==null) return 0;
+        sum=(sum*10)+root.val;
+        if(root.left==null && root.right==null) return sum;
+        return helper(root.left,sum)+helper(root.right,sum);
     }
     public int sumNumbers(TreeNode root) {
-        List<String> ans = new ArrayList<>();
-        if(root==null) return 0;
-        helper(root,ans,"");
-        int res = 0;
-        for(String i:ans) res+=(Integer.parseInt(i));
-        return res;
+        return helper(root,0);
+        // List<String> ans = new ArrayList<>();
+        // if(root==null) return 0;
+        // helper(root,ans,"");
+        // int res = 0;
+        // for(String i:ans) res+=(Integer.parseInt(i));
+        // return res;
     }
 }
