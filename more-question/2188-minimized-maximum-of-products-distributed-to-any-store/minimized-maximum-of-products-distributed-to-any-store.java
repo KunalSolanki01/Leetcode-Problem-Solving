d077e30f85d183bef45466d8771281c9dd556d77
Kunal Solanki <kunalsol2005@gmail.com>
2026-08-17 12:49:17 +0530

    Time: 113 ms (5.99%) | Memory: 81 MB (89.42%) - LeetSync

---
+++ more-question/2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minimizedMaximum(int k, int[] nums) {
+        int n = nums.length;
+        int l = 0,h = 0;
+        for(int i:nums){
+            h = Math.max(h,i);
+        }
+        while(l<=h){
+            int mid = l+(h-l)/2;
+            if(isP(nums,k,mid)) h = mid-1;
+            else l = mid+1;
+        }
+        return l;
+    }
+    public boolean isP(int[] nums,int k,int mid){
+        int count = 0;
+        if(mid==0) return false;
+        for(int i:nums){
+            count+=Math.ceil((double)i/(mid));
+        }
+        return count<=k;
+    }
+}
