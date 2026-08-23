6e3f38dcc78f3ed35d459f3dbced6dbe86a09e20
Kunal Solanki <kunalsol2005@gmail.com>
2026-08-23 08:58:00 +0530

    Time: 8 ms (53.28%) | Memory: 47.2 MB (10.95%) - LeetSync

---
+++ more-question/2039-sum-game/sum-game.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean sumGame(String num) {
+        int n = num.length();
+        int sum = 0,qm = 0;
+        for(int i=0;i<n;i++){
+            char c = num.charAt(i);
+            int val = (c!='?')?c-'0':0;
+            if(i<n/2){
+                sum+=val;
+                if(c=='?')qm++;
+            }
+            else{
+                sum-=val;
+                if(c=='?')qm--;
+            }
+        }
+        return (2*sum+qm*9)!=0;
+    }
+}
