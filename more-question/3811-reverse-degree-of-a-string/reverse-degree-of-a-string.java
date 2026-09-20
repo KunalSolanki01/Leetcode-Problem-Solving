800314cd828dfbb6b33c441180feb62de1569f65
Kunal Solanki <kunalsol2005@gmail.com>
2026-09-20 13:33:46 +0530

    Time: 1 ms (100.00%) | Memory: 44.1 MB (34.29%) - LeetSync

---
+++ more-question/3811-reverse-degree-of-a-string/reverse-degree-of-a-string.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int reverseDegree(String s) {
+        int sum = 0,i = 1;
+        for(char c:s.toCharArray()){
+            sum+=(26-(c-'a'))*i;
+            i++;
+        }
+        return sum;
+    }
+}
